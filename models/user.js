6b95e9f512d6a2fbdec26a3a1124bf2a6d3f1d2f
gabriel963y <gabriel963y@gmail.com>
2026-05-30 00:14:03 -0300

    agregar modelos y archivos de pnpm

---
+++ models/user.js
@@ -0,0 +1,37 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class User extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+
+        }
+    }
+    User.init(
+        {
+            nickName: {
+                type: DataTypes.STRING,
+                primaryKey: true,
+                autoIncrement: false,
+                allowNull: false
+            },
+            email: {
+                type: DataTypes.STRING,
+                unique: true,
+                allowNull: false
+            },
+            name: DataTypes.STRING,
+            surname: DataTypes.STRING,
+            followers: DataTypes.INTEGER,
+        },
+        {
+            sequelize,
+            modelName: 'User',
+        }
+    );
+    return User;
+};
