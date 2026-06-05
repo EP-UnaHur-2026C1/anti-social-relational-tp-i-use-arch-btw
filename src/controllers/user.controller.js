9bf09b0e0a8fbb533089193849b4a532aed31822
Orejas 2016 <joaomasgosandoval@gmail.com>
2026-06-05 01:03:46 -0300

    User controller terminado

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -15,11 +15,84 @@ const getAllUsers = async (req, res) => {
 
 const getUserByNickName = async (req, res) => {
     try {
-        
+        const user = await User.findByPk(req.params.nickName);
+
+        if (!user) {
+            return res.status(404).json({
+                message: 'Usuario no encontrado',
+            });
+        }
+
+        res.status(200).json(user);
+
     } catch (error) {
         res.status(500).json({
             message: 'Error al obtener usuario.',
             error: error.message,
         });
     }
+};
+
+const createUser = async (req, res) => {
+    try {
+        const user = await User.create(req.body);
+
+        res.status(201).json(user);
+    } catch(error) {
+        res.status(500).json({
+            message: 'Error al crear usuario.',
+            error: error.message,
+        });
+    }
+};
+
+const updateUser = async (req, res) => {
+    try {
+        const user = await User.findByPk(req.params.nickName);
+
+        if(!user) {
+            return res.status(404).json({
+                message: 'usuario no encontrado.'
+            });
+        }
+
+        await user.update(req.body);
+
+        res.status(200).json(user);
+
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error al actualizar usuario.',
+            error: error.message,
+        });
+    }
 }
+
+const deleteUser = async (req, res) => {
+    try {
+        const user = await User.findByPk(req.params.nickName);
+
+        if(!user) {
+            return res.status(404).json({
+                message: 'usuario no encontrado.'
+            });
+        }
+        await user.destroy();
+        res.status(204).send();
+
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error al eliminar usuario.',
+            error: error.message,
+        });
+    }
+}
+
+
+module.exports = {
+    getAllUsers,
+    getUserByNickName,
+    deleteUser,
+    createUser,
+    updateUser,
+};
