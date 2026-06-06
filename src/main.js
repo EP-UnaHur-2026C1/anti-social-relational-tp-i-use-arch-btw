eeeb607293847936b58c5cf06776a8fd020102c9
ZoeAby <zoe.damore@estudiantes.unahur.edu.ar>
2026-06-05 21:23:12 -0300

    Actualización de rutas

--- src/main.js
+++ src/main.js
@@ -10,6 +10,9 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
+app.use('/api/posts', postRoutes);
+app.use('/api/comments', commentRoutes);
+app.use('/api/tags', tagRoutes);
 
 app.listen(PORT, async () => {
     await db.sequelize.sync();
